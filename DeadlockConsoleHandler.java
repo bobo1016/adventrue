0cb23b452b33c98414d8c5bc01b10fc4fdcdd86f
bobo1016 <pengpengdoreen@gmail.com>
2016-05-13 00:30:18 -0400

    initial frame

--- DeadlockConsoleHandler.java
+++ DeadlockConsoleHandler.java
@@ -4,7 +4,9 @@ package project;
  * Created by pengbo on 2016-05-13.
  */
 
+import sun.jvm.hotspot.ui.DeadlockDetectionPanel;
+
 import  java.lang.management.*;
 
-public class DeadlockConsoleHandler {
+public class DeadlockConsoleHandler implements DeadlockDetectionPanel{
 }
